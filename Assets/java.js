7ef7e74002e4a10e07150bbcf1b0d9982c62e748
Malcolm Franklin <mac.mac5@yahoo.com>
2024-04-10 18:19:44 -0500

    check whats up wiht index html when you inspect it

--- Assets/java.js
+++ Assets/java.js
@@ -30,12 +30,12 @@ function getAnimalInfo() {
     });
 }
 
-searchButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    event.stopPropagation();
-    getAnimalInfo();
+// searchButton.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     event.stopPropagation();
+//     getAnimalInfo();
 
-});
+// });
 
 /** ------------------------------------------------------------------------*/ 
 
@@ -55,3 +55,42 @@ searchButton.addEventListener("click", function (event) {
 //     .then(function (data) {
 //         console.log(data);
 //     });
+
+
+// For mobile menu 
+
+// const burgerIcon = document.querySelector('#burger');
+// const navbarMenu = document.querySelector('#nav-links');
+
+// burgerIcon.addEventListener('click', () => {
+//     navbarMenu.classList.toggle('is-active')
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+  
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+  
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+  
+      });
+    });
+  
+  });
+
+  const mobileBuger = document.querySelector("#burger");
+  const navbarLinks = document.querySelector("#nav-links");
+
+  mobileBuger.addEventListener('click', () => {
+    navbarLinks.classList.toggle('is-active');
+  });
